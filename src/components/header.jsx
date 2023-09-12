04f4e2a85f33357592a6bddb711f46e67f0cfccd
UnCompa <brandonddxd@gmail.com>
2023-09-12 11:57:38 -0500

    Nav mal hecho

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,9 +1,11 @@
+import NavBar from './../components/nav'
 import Logo from './../assets/1.png'
 function Header(){
     console.log(Logo);
     return (
         <header className="h-24 bg-slate-900 w-full">
             <img src={Logo} alt="" className='h-full w-48 object-cover'/>
+            <NavBar/>
         </header>
     )
 }
---
+++ src/components/nav.jsx
@@ -0,0 +1,9 @@
+function NavBar(){
+    return (
+        <nav className="h-full bg-slate-700 ">
+            <button>Hola</button>
+        </nav>
+    )
+}
+
+export default NavBar;
