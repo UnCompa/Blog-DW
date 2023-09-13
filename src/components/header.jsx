65df7fd1353e5103734892d53ef69864e3acab5b
UnCompa <brandonddxd@gmail.com>
2023-09-13 11:54:17 -0500

    Mejorando el nav(no funciona el componente)

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,11 +1,11 @@
-import NavBar from './../components/nav'
+import Nav from './../components/nav'
 import Logo from './../assets/1.png'
 function Header(){
     console.log(Logo);
     return (
-        <header className="h-24 bg-slate-900 w-full">
-            <img src={Logo} alt="" className='h-full w-48 object-cover'/>
-            <NavBar/>
+        <header className="h-24 bg-slate-900 w-full flex justify-between">
+                <img src={Logo} alt="" className='h-full w-48 object-cover'/>
+            <Nav/>
         </header>
     )
 }
--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -1,9 +1,26 @@
+let Boton = document.getElementById('btn')
+        Boton.addEventListener('click',()=>{
+        const menu = document.getElementById('menu')
+        if(menu.classList.contains('hidden')){
+            menu.classList.remove('hidden')
+            menu.classList.add('block')
+        } else {
+            menu.classList.remove('block')
+            menu.classList.add('hidden')
+        }
+    })
 function NavBar(){
     return (
-        <nav className="h-full bg-slate-700 ">
-            <button>Hola</button>
+        <nav className="h-24 w-48  flex justify-center items-center relative">
+            <button id="btn" className="text-white text-3xl bg-slate-700 px-6 py-2">=</button>
+            <div id="menu" className="bg-slate-700 h-24 absolute top-24 left-0 w-full hidden">
+                <ul>
+                    <li className="bg-slate-800 m-2 p-2"><a className="text-white" href="">Inicio</a></li>
+                    <li className="bg-slate-800 m-2 p-2"><a className="text-white" href="">Contenido</a></li>
+                    <li className="bg-slate-800 m-2 p-2"><a className="text-white" href="">Contacto</a></li>
+                </ul>
+            </div>
         </nav>
     )
 }
-
 export default NavBar;
